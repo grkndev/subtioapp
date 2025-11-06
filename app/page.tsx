5669b9f023f0e0366ac0da44d413d8c1b2bc6bce
grkndev <grkndev@grkndevs-MacBook-Air.local>
2025-11-06 18:04:55 +0200

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,59 @@
 import Image from "next/image";
+import BgIns from "@/components/svg/bgins";
+import Arrow from "@/components/svg/arrow";
+import { Handlebars } from "@/components/ui/handlebars";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className=" flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="absolute top-0 left-0 w-screen h-full">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={"/bglight.png"}
+          alt="bglight"
+          className="w-full object-fill absolute"
+          width={1920}
+          height={1080}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+        <BgIns className="w-full" />
+      </div>
+
+      <main className="flex gap-10 min-h-screen w-full max-w-fit flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black ">
+        <div className="flex items-center justify-center w-full gap-16">
+          <Arrow className="-rotate-65" />
+          <div className="w-full flex flex-col items-center justify-center gap-8">
+            <div className="w-full flex flex-col items-center justify-center gap-4">
+              <h1 className="font-bold text-7xl text-center">
+                Add <span className="underline">subtitles</span> to your
+              </h1>
+              <div className="flex items-center justify-center w-full gap-4">
+                <Handlebars>
+                  <span className="text-7xl font-bold">Video</span>
+                </Handlebars>
+                <h1 className="font-bold text-7xl text-center">in seconds</h1>
+              </div>
+            </div>
+            <Button className="text-2xl w-1/4 py-6">
+              Try now <ArrowRight size={24}/>
+            </Button>
+            <h6 className="text-center text-lg text-gray-500">
+              Upload your video and get subtitles in any language you want, the
+              way you want them.
+            </h6>
+          </div>
+          <Arrow className="rotate-y-180 rotate-65" />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        {/* <div className="  items-center justify-center flex  p-1  bg-gradient-to-tl from-subtio-gold via-transparent via-50% to-subtio-gold rounded-2xl ">
+          <Image
+            src={"/bgvideo.png"}
+            alt="bgvideo"
+            width={1920}
+            height={1080}
+            className=" object-cover h-[40vh] aspect-video w-fit  rounded-xl"
+          />
+        </div> */}
       </main>
     </div>
   );
